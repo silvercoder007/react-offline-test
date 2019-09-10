085b028dfe5af7f83e89bda9b6afa19f3efb8441
Ali K <akayaniwork@outlook.com>
2019-09-10 21:40:17 +0100

    feature/homepage - added menu tests and refactored component

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 import './menu.scss';
 
 export default function Menu({menuItems}) {
-
-    let GreenEnergy = '';
-    let About = '';
-    let Careers = '';
-    let ContactUs = '';
-
-    [GreenEnergy, About, Careers, ContactUs] = menuItems;
+    if (!menuItems && menuItems.length) return null;
     return (
+        <>
+        {menuItems.length && (
         <header class="header">
             <a href="" class="logo">CSS Nav</a>
             <input class="menu-btn" type="checkbox" id="menu-btn" />
             <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
             <ul class="menu">
-                <li><a href="#work">{GreenEnergy}</a></li>
-                <li><a href="#about">{About}</a></li>
-                <li><a href="#careers">{Careers}</a></li>
-                <li><a href="#contact">{ContactUs}</a></li>
+                <li><a href="#work">{menuItems[0]}</a></li>
+                <li><a href="#about">{menuItems[1]}</a></li>
+                <li><a href="#careers">{menuItems[2]}</a></li>
+                <li><a href="#contact">{menuItems[3]}</a></li>
             </ul>
-</header>
+        </header>
+        )}
+        </>
     );
 }
---
+++ src/components/menu.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import menu from './menu.jsx';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+import { shallow, mount } from 'enzyme';
+
+describe('<menu />', () => {
+    let props;
+    beforeEach(() => {
+        props = {
+            menuItems: ['Green Energy', 'About', 'Careers', 'Contact Us']
+        }
+    })
+    it('should render the component', () => {
+        const wrapper = shallow(<menu {...props} />);
+       expect(wrapper).toHaveLength(1);
+    });
+    it('should have the correct props set', () => {
+        const wrapper = shallow(<menu {...props}  />);
+        const expectedProps = [ 'Green Energy', 'About', 'Careers', 'Contact Us' ];
+        expect(wrapper.props().menuItems).toEqual(expectedProps);
+    });
+});
