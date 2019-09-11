2c73acf0c7a16a144d3f287b383d6586f45e5c76
Ali K <akayaniwork@outlook.com>
2019-09-11 09:24:17 +0100

    feature/homepage - added chart tests

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,39 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
-import style from './chart.scss';
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+import style from "./chart.scss";
 
-function processData (array) {
+function processData(array) {
   const anotherArray = [];
-  const newData = array.map((fuel) => {
-  const newFuelObject = {
-    name: fuel.fuel,
-    percentage: fuel.perc
-  }
-  anotherArray.push(newFuelObject);
-});
-return anotherArray;
-};
+  const newData = array.map(fuel => {
+    const newFuelObject = {
+      name: fuel.fuel,
+      percentage: fuel.perc
+    };
+    anotherArray.push(newFuelObject);
+  });
+  return anotherArray;
+}
 
-export default function Chart ({data}) {
-
-    const { generationmix } = data;
-    const dataArray = processData(generationmix && generationmix);
-    return (
+export default function Chart({ data }) {
+  const { generationmix } = data;
+  const dataArray = processData(generationmix && generationmix);
+  return (
     <section>
-      <LineChart
-        width={800}
-        height={500}
-        data={dataArray}
-      >
+      <LineChart width={800} height={500} data={dataArray}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="percentage" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line
+          type="monotone"
+          dataKey="percentage"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
       </LineChart>
-      </section>
-    );
+    </section>
+  );
 }
---
+++ src/components/chart/chart.test.js
@@ -0,0 +1,80 @@
+import React from "react";
+import chart from "./chart.jsx";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+import { shallow, mount } from "enzyme";
+
+describe("<chart />", () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      data: {
+        from: "2019-09-11T07:30Z",
+        to: "2019-09-11T08:00Z",
+        generationmix: [
+          {
+            fuel: "biomass",
+            perc: 10.3
+          },
+          {
+            fuel: "coal",
+            perc: 2.1
+          },
+          {
+            fuel: "imports",
+            perc: 12.2
+          },
+          {
+            fuel: "gas",
+            perc: 29.1
+          },
+          {
+            fuel: "nuclear",
+            perc: 29.9
+          },
+          {
+            fuel: "other",
+            perc: 0.8
+          },
+          {
+            fuel: "hydro",
+            perc: 2.3
+          },
+          {
+            fuel: "solar",
+            perc: 2.7
+          },
+          {
+            fuel: "wind",
+            perc: 10.6
+          }
+        ]
+      }
+    };
+  });
+  it("should render the component", () => {
+    const wrapper = shallow(<chart {...props} />);
+    console.log(wrapper.debug());
+    console.log(wrapper.props().data.generationmix);
+    expect(wrapper).toHaveLength(1);
+  });
+  it("should have props", () => {
+    const wrapper = shallow(<chart {...props} />);
+    expect(wrapper.props()).toBeDefined();
+  });
+  it("should contain an array of data", () => {
+    const wrapper = shallow(<chart {...props} />);
+    expect(wrapper.props().data.generationmix).toEqual([
+      { fuel: "biomass", perc: 10.3 },
+      { fuel: "coal", perc: 2.1 },
+      { fuel: "imports", perc: 12.2 },
+      { fuel: "gas", perc: 29.1 },
+      { fuel: "nuclear", perc: 29.9 },
+      { fuel: "other", perc: 0.8 },
+      { fuel: "hydro", perc: 2.3 },
+      { fuel: "solar", perc: 2.7 },
+      { fuel: "wind", perc: 10.6 }
+    ]);
+  });
+});
